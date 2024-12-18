299531cc55a6fa7f4b8b5c37aa921d32d4937d65
python developer <123306614+GamilaCoding@users.noreply.github.com>
2024-12-18 18:41:35 +0200

    Delete src directory

--- src/Components/UIElements/TextButton.jsx
+++
@@ -1,9 +0,0 @@
-const TextButton = ({ text  , ClickHandler}) => {
-  return (
-    <button  onClick={ClickHandler} className="py-2 px-4 rounded-md text-blue-light font-bold ">
-      {text}
-    </button>
-  );
-};
-
-export default TextButton;
